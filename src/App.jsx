f725e9c338e2a56e95622411a88bdb6ff0bbe534
ahmedsalah-iti <ahmedsalah.iti@gmail.com>
2025-05-07 17:56:43 +0300

    starting lab1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import myInfo from './info'
+function App(){
+  return (
+    <>
+    <h1>{myInfo.name}</h1>
+    </>
+  )
+}
+
+export default App;
---
+++ src/info.js
@@ -0,0 +1,41 @@
+const myInfo = {
+    name:"Ahmed Salah",
+    about:"test",
+    skills:[
+        {
+            name:"php",
+            percent:90,
+        },
+        {
+            name:"html",
+            percent:5,
+        }
+    ],
+    projects:[
+        {
+            name:"Cafeteria",
+            description:"eCommerce Website using native PHP",
+            githubLink:"https://github.com/ahmedsalah-iti/php_project"
+        },
+        {
+            name:"test1",
+            description:"test1 project",
+            githubLink:"#"
+        },
+        {
+            name:"test2",
+            description:"test2 project",
+            githubLink:"#"
+        }
+    ],
+    socials:[
+        {
+            name:"facebook",
+            link:"https://www.facebook.com/"
+        },        {
+            name:"twitter",
+            link:"https://www.twitter.com/"
+        },
+    ]
+}
+export default myInfo;
